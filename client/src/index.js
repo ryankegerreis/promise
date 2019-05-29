21785b4d307fa379433776230086421389dfc230
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-05-29 12:13:35 -0400

    s

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'bulma/css/bulma.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import './index.scss';
+import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App.jsx';
 // import registerServiceWorker from './registerServiceWorker';
